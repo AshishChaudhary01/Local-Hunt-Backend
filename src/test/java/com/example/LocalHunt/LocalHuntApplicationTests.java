906aea8e67d1839b5334b5a1a2c5c44edf2f9a12
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-11-12 08:29:03 +0545

    Setup project & add dependencies

---
+++ src/test/java/com/example/LocalHunt/LocalHuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LocalHunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalHuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
